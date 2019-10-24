d66f4cf8b436749b81819c31253143d6942b13dd
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-10-23 20:40:12 -0400

    completion of format/styling of menu component

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -13,59 +13,68 @@ function Menu() {
       <MenuCarousel />
       <div className="menu-content">
         <h1 style={{ fontSize: 50 }}> Catering Menu:</h1>
+        <br></br>
+        <div className="container">
+          <div className="newColumns">
+            <h3 className="main-dishes" style={{ fontSize: 30 }}>
+              Main:
+            </h3>
+            <ul id="main">
+              <li>Chicken Wings quantities of 50 pcs+</li>
+              <li>Chicken Strips quantities of 50pcs+</li>
+              <li>Likkle Bits in tray sizes: Medium or Large</li>
+            </ul>
+          </div>
 
-        <h3 className="main-dishes" style={{ fontSize: 45 }}>
-          Main:
-        </h3>
-        <ul>
-          <li>Chicken Wings quantities of 50 pcs+</li>
-          <li>Chicken Strips quantities of 50pcs+</li>
-          <li>Likkle Bits in tray sizes: Medium or Large</li>
-        </ul>
+          <div className="newColumns">
+            <h3 className="side-dishes" style={{ fontSize: 30 }}>
+              Sides:
+            </h3>
+            <ul>
+              <li>South-Side Chicken Wraps</li>
+              <li>French Fries</li>
+              <li>South-Side Caesar Fries</li>
+              <li>South-Side Chicken Sandwich</li>
+              <li>South-Side Chicken Sliders</li>
+            </ul>
+          </div>
 
-        <h3 className="side-dishes" style={{ fontSize: 45 }}>
-          Sides:
-        </h3>
-        <ul>
-          <li>South-Side Chicken Wraps</li>
-          <li>French Fries</li>
-          <li>South-Side Caesar Fries</li>
-          <li>South-Side Chicken Sandwich</li>
-          <li>South-Side Chicken Sliders</li>
-        </ul>
+          <div className="newColumns">
+            <h3 className="sauces" style={{ fontSize: 30 }}>
+              Available Sauces:
+            </h3>
+            <ul id="gotsauce">
+              <li>South-Side Sauce</li>
+              <li>Dirty South</li>
+              <li>IG Honey</li>
+              <li>Bishop Brew</li>
+              <li>BBQ</li>
+              <li>Lemon Pepper (Wet or Dry)</li>
+              <li>Chipotle Mango</li>
+              <li>Super South-Side(HOT!!!)</li>
+            </ul>
+          </div>
 
-        <h3 className="sauces" style={{ fontSize: 45 }}>
-          Available Sauces:
-        </h3>
-        <p>* on or on the side of wings*</p>
-        <ul>
-          <li>South-Side Sauce</li>
-          <li>Dirty South</li>
-          <li>IG Honey</li>
-          <li>Bishop Brew</li>
-          <li>BBQ</li>
-          <li>Lemon Pepper (Wet or Dry)</li>
-          <li>Chipotle Mango</li>
-          <li>Super South-Side(HOT!!!)</li>
-        </ul>
+          <div className="newColumns">
+            <h3 className="bottled-sauces" style={{ fontSize: 30 }}>
+              Bottled Sauces For Sale:
+            </h3>
+            <ul id="botsauce">
+              <li>South-Side Sauce</li>
+              <li> Dirty South Sauce </li>
+              <li> IG Honey Sauce</li>
+              <li>Bishop Brew</li>
+            </ul>
+          </div>
 
-        <h3 className="bottled-sauces" style={{ fontSize: 45 }}>
-          Bottled Sauces For Sale:
-        </h3>
-        <ul>
-          <li>South-Side Sauce</li>
-          <li> Dirty South Sauce </li>
-          <li> IG Honey Sauce</li>
-          <li>Bishop Brew</li>
-        </ul>
-
-        <p>
-          For further inquiries, bookings and purchasing of services/bottled
-          goods inquire here:{" "}
-          <Link to="/contact" className="link-contact">
-            Contact
-          </Link>{" "}
-        </p>
+          <p>
+            For further inquiries, bookings and purchasing of services/bottled
+            goods inquire{" "}
+            <Link to="/contact" className="link-contact">
+              <b>here</b>
+            </Link>{" "}
+          </p>
+        </div>
       </div>
 
       <Footer />
