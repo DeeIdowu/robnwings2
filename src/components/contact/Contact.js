9166ca89787d42b80ea6af7c33b8eb9d8d8393a5
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-10-27 09:13:32 -0400

    adding contact page

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+import "./contact.css";
+
+function Contact() {
+  return (
+    <div>
+      <Header />
+      <div className="form-section"></div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Contact;
