9b3da8f70731911a3aaa96f0d91b528a0972c04d
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-11-05 18:46:01 -0500

    editing contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,8 +6,32 @@ import "./contact.css";
 class Contact extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+      emailSent: null
+    };
   }
+  //handling change
+  handleChange = event => {
+    console.log(event);
+
+    const target = event.target;
+    const name = target.name;
+    const email = target.email;
+    const subject = target.subject;
+    const message = target.message;
+
+    this.setState({
+      [name]: target,
+      [email]: target,
+      [subject]: target,
+      [message]: target
+    });
+  };
+
   render() {
     return (
       <div>
@@ -15,28 +39,46 @@ class Contact extends React.Component {
         <br></br>
         <br></br>
         <h1>Contact Us:</h1>
-        <form className="contact-form">
-          <label className="name">
+        <form onSubmit={this.handleSubmit} className="contact-form">
+          <label
+            className="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          >
             Name: <input type="text"></input>
           </label>
           <br></br>
           <br></br>
-          <label className="email">
+          <label
+            className="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          >
             Email: <input type="text"></input>
           </label>
           <br></br>
           <br></br>
-          <label className="subject">
+          <label
+            className="subject"
+            value={this.state.subject}
+            onChange={this.handleChange}
+          >
             Subject: <input type="text"></input>
           </label>
           <br></br>
           <br></br>
-          <label>Message:</label>
+          <label value={this.state.message} onChange={this.handleChange}>
+            Message:
+          </label>
           <br></br>
           <textarea className="textarea"></textarea>
           <br></br>
           <br></br>
           <input id="Submit" type="submit" value="Submit"></input>
+
+          {this.state.emailSent === true && alert("E-mail sent, Thank you!!!")}
+          {this.state.emailSent === false &&
+            alert("Error, E-mail did not send!!")}
         </form>
         <br></br>
         <br></br>
